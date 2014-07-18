8950a7c56104f3837dcb5e917acc19dad45de25c
dbaker <d-_-baker@hotmail.com>
2014-07-18 19:38:59 +1000

    bot_things improvements, including new wordlist

--- bot_things/bot_things.py
+++ bot_things/bot_things.py
@@ -1,9 +1,14 @@
-
+# encoding: utf-8
+from __future__ import unicode_literals
 import random
 import os
+from datetime import date
 
-words = open("resources/everyword.txt").read().split()
-count = 0
+# this should really be in a class but oh well
+
+NOUNS = open("resources/nouns.txt").read().split()
+ADJECTIVES = open("resources/adjectives.txt").read().split()
+EVERYWORD = open("resources/everyword.txt").read().split()
 
 def gen():
 
@@ -12,31 +17,83 @@ def gen():
     else:
         tweeted_words = open("resources/tweeted_words.txt", "w+") # create file
 
-    # keep generating words until one that hasn't been picked before turns up
-    word = random.choice(words).strip()
-    while word in tweeted_words:
-        word = random.choice(words).strip()
+    # bot variation
+    special_prefixes = ("computer", "robot", "processor", "syntax", "compiler", "algorithm")
 
-    if (os.path.exists("resources/tweeted_words.txt")):
-        tweeted_words = open("resources/tweeted_words.txt", "a") # append to file
+    word = ""
+    phrase = ""
+
+    # high chance of "{subject} {noun}"
+    if random.random() > 0.02:
+        word = random.choice(NOUNS).strip()
+
+        for i in range(0, 100):
+            if word in tweeted_words:
+                word = random.choice(NOUNS).strip()
+
+                # give up and use everyword list if we sort-of run out of words
+                if i == 99:
+                    last_resort(word, tweeted_words)
+
+        # high chance of using "bot"
+        if random.random() > 0.02:
+            phrase = "bot " + word
+        # small chance of using something else
+        else:
+            phrase = random.choice(special_prefixes) + " " + word
+
+
+    # small chance of "{adjective} {subject}"
     else:
-        tweeted_words = open("resources/tweeted_words.txt", "w") # create file
+        word = random.choice(ADJECTIVES).strip()
+
+        for i in range(0, 100):
+            if word in tweeted_words:
+                word = random.choice(ADJECTIVES).strip()
+
+                # give up and use everyword list if we sort-of run out of words
+                if i == 99:
+                    last_resort(word, tweeted_words)
+
+        # high chance of using "bot"
+        if random.random() > 0.02:
+            phrase = word + " bot"
+        # small chance of using something else
+        else:
+            phrase = word + " " + random.choice(special_prefixes)
+
+    # append to file if it exists
+    if (os.path.exists("resources/tweeted_words.txt")):
+        tweeted_words = open("resources/tweeted_words.txt", "a")
+    # create file otherwise
+    else:
+        tweeted_words = open("resources/tweeted_words.txt", "w")
 
     # add latest word to file
     tweeted_words.write(word + "\n")
 
-    # bot variation
-    special_prefixes = ("computer ", "robot ", "processor ", "syntax ", "compiler ", "algorithm ")
-
-    phrase = ""
-
-    if random.random() < 0.02:
-        phrase = random.choice(special_prefixes) + " " + word
-    else:
-        phrase = "bot " + word
-
-
     return phrase
+
+
+def last_resort(word):
+    word = random.choice(EVERYWORD).strip()
+    for i in range(0, 100):
+        if word in tweeted_words:
+            word = random.choice(EVERYWORD).strip()
+
+            # even everyword gives up sometimes
+            if i == 99:
+                log("bot apocalypse")
+                return
+
+
+def log(message):
+    today = "{day}/{month}/{year}".format(
+        day=date.today().day, 
+        month=date.today().month, 
+        year=date.today().year)
+
+    print "{d}: {m}".format(d=today, m=message)
 
 
 if __name__ == "__main__":
