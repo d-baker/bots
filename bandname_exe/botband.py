eba69b663499feb91f7cbde09cfe489209dc290f
dbaker <d-_-baker@hotmail.com>
2014-07-20 13:41:43 +1000

    new format for bandname.exe

--- bandname_exe/botband.py
+++ bandname_exe/botband.py
@@ -1,13 +1,57 @@
 import random
 
 def gen():
+    nouns = list(open("resources/nouns.txt", "r").read().split())
+    adjectives = list(open("resources/adjectives.txt", "r").read().split())
+
     of_prefix = list(open("resources/of_prefixes.txt", "r").read().split())
     of_postfix = list(open("resources/of_postfixes.txt", "r").read().split())
     the_prefix = list(open("resources/the_prefixes.txt", "r").read().split())
     the_postfix = list(open("resources/the_postfixes.txt", "r").read().split())
 
-    options = (random.choice(of_prefix) + " of " + random.choice(of_postfix), random.choice(the_prefix) + " the " + random.choice(the_postfix))
-    text = random.choice(options)
+
+    normal_options = (
+
+        "{noun} of {nouns}".format(
+            noun = random.choice(of_prefix),
+            nouns = random.choice(of_postfix)
+        ),
+
+        "{verb} the {noun}".format(
+            verb = random.choice(the_prefix),
+            noun = random.choice(the_postfix)
+        )
+
+    )
+
+
+    silly_options = (
+
+        "{noun} of {adjective} {nouns}".format(
+            noun = random.choice(of_postfix),
+            adjective = random.choice(adjectives),
+            nouns = random.choice(nouns)
+        ),
+
+        "{verb} the {adjective} {noun}".format(
+            verb = random.choice(the_prefix),
+            adjective = random.choice(adjectives),
+            noun = random.choice(nouns)
+        )
+
+    )
+
+
+    text = ""
+
+    if random.random() < 0.08:
+        text = random.choice(silly_options)
+    else:
+        text = random.choice(normal_options)
 
     return text
+
+if __name__ == "__main__":
+    for i in range (0, 10):
+        print gen()
 
