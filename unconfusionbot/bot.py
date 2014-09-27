5701d577ccc2e0f0f9c3606d0dcc78bdefee38f0
dbaker <d-_-baker@hotmail.com>
2014-09-27 13:41:55 +1000

    refactor to use twitterbot

--- unconfusionbot/bot.py
+++ unconfusionbot/bot.py
@@ -1,8 +1,8 @@
-# encoding: utf-8
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
 import json
-import tweepy
 import re
 import time
 
@@ -12,31 +12,139 @@ import requests
 import pytz
 
 from resources.config import CONSUMER_KEY, CONSUMER_SECRET, TOKEN, SECRET
-import twitter
 
-# THANKYOU www.geonames.org for the data!
+from twitterbot import TwitterBot
 
-class UnconfusionBot:
+class UnconfusionBot(TwitterBot):
+    def bot_init(self):
+        """
+        Initialize and configure your bot!
 
-    def __init__(self):
-        auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-        auth.set_access_token(TOKEN,SECRET)
-        self.api = tweepy.API(auth)
+        Use this function to set options and initialize your own custom bot
+        state (if any).
+        """
 
-    # TODO only retrieves one mention!
-    # also need to check for age of tweet
-    def get_mentions(self):
-        for mention in tweepy.Cursor(self.api.mentions_timeline).items(1):
-            return mention
+        ############################
+        # REQUIRED: LOGIN DETAILS! #
+        ############################
+        self.config['api_key'] = CONSUMER_KEY
+        self.config['api_secret'] = CONSUMER_SECRET
+        self.config['access_key'] = TOKEN
+        self.config['access_secret'] = SECRET
 
-    def get_username(self, mention):
-        return mention.user.screen_name
+
+        ######################################
+        # SEMI-OPTIONAL: OTHER CONFIG STUFF! #
+        ######################################
+
+        # how often to tweet, in seconds
+        self.config['tweet_interval'] = 30 * 60     # default: 30 minutes
+
+        # use this to define a (min, max) random range of how often to tweet
+        # e.g., self.config['tweet_interval_range'] = (5*60, 10*60) # tweets every 5-10 minutes
+        self.config['tweet_interval_range'] = None
+
+        # only reply to tweets that specifically mention the bot
+        self.config['reply_direct_mention_only'] = False
+
+        # only include bot followers (and original tweeter) in @-replies
+        self.config['reply_followers_only'] = True
+
+        # fav any tweets that mention this bot?
+        self.config['autofav_mentions'] = False
+
+        # fav any tweets containing these keywords?
+        self.config['autofav_keywords'] = []
+
+        # follow back all followers?
+        self.config['autofollow'] = False
+
+
+        ###########################################
+        # CUSTOM: your bot's own state variables! #
+        ###########################################
+        
+        # If you'd like to save variables with the bot's state, use the
+        # self.state dictionary. These will only be initialized if the bot is
+        # not loading a previous saved state.
+
+        # self.state['butt_counter'] = 0
+
+        # You can also add custom functions that run at regular intervals
+        # using self.register_custom_handler(function, interval).
+        #
+        # For instance, if your normal timeline tweet interval is every 30
+        # minutes, but you'd also like to post something different every 24
+        # hours, you would implement self.my_function and add the following
+        # line here:
+        
+        # self.register_custom_handler(self.my_function, 60 * 60 * 24)
+
+
+    def on_scheduled_tweet(self):
+        """
+        Make a public tweet to the bot's own timeline.
+
+        It's up to you to ensure that it's less than 140 characters.
+
+        Set tweet frequency in seconds with TWEET_INTERVAL in config.py.
+        """
+        # text = function_that_returns_a_string_goes_here()
+        # self.post_tweet(text)
+
+        pass
+
+    def on_mention(self, tweet, prefix):
+        tweet_time = tweet.created_at # tweet timestamp
+        now = datetime.utcnow()
+        diff = now - tweet_time # tweet age
+
+        # only reply to mentions in last 2 mins
+        if diff.seconds <= 120:
+            location = self.get_location(tweet.text)
+
+            mention = re.sub(r'(^|[^@\w])@(\w{1,15})\b', "", tweet.text)
+
+            if "time" in mention:
+                text = self.get_timezone(location)
+            elif "temp" in mention:
+                text = self.get_temp(location) + "°C"
+
+            self.post_tweet(prefix + ' ' + text, reply_to=tweet)
+
+
+    def on_timeline(self, tweet, prefix):
+        """
+        Defines actions to take on a timeline tweet.
+
+        tweet - a tweepy.Status object. You can access the text with
+        tweet.text
+
+        prefix - the @-mentions for this reply. No need to include this in the
+        reply string; it's provided so you can use it to make sure the value
+        you return is within the 140 character limit with this.
+
+        It's up to you to ensure that the prefix and tweet are less than 140
+        characters.
+
+        When calling post_tweet, you MUST include reply_to=tweet, or
+        Twitter won't count it as a reply.
+        """
+        # text = function_that_returns_a_string_goes_here()
+        # prefixed_text = prefix + ' ' + text
+        # self.post_tweet(prefix + ' ' + text, reply_to=tweet)
+
+        # call this to fav the tweet!
+        # if something:
+        #     self.favorite_tweet(tweet)
+
+        pass
 
     # TODO needs to be much more flexible - can't really assume last word
-    # in tweet will always be the location
+    # in tweet will always be the location, or that it's only a single word
     def get_location(self, mention):
-        tweet_strings = list(re.split('\s|,|/', mention.text))
-        return tweet_strings[len(tweet_strings)-1]
+        tweet_strings = list(re.split('\s|,|/', mention))
+        return tweet_strings[len(tweet_strings)-1].capitalize()
 
     def get_lat_long(self, location):
         url = "http://api.geonames.org/searchJSON?name={}&username=dbaker".format(location)
@@ -82,39 +190,7 @@ class UnconfusionBot:
 
         return False
 
-    ##########################################################################
-
-    def log(self, message):
-        time = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
-        print ("{t} | {m}".format(t = time, m = message))
-
-    def tweet(self, text, mention):
-        status = "@{u} {t}".format(u = mention.user.screen_name, t = text)
-        in_reply_to = mention.id
-
-        s = self.api.update_status(status, in_reply_to)
-
-        self.log("tweeted \"{t}\"".format(t = status))
-
-    def run(self):
-        while True:
-            try:
-                mention = self.get_mentions()
-                mention_text = mention.text.replace("@tempntime_bot", "")
-                username = self.get_username(mention)
-                location = self.get_location(mention)
-
-                if "time" in mention_text:
-                    self.tweet(self.get_timezone(location), mention)
-                elif "temp" in mention_text:
-                    self.tweet(self.get_temp(location) + "°C", mention)
-
-                time.sleep(10)
-
-            except tweepy.TweepError:
-                time.sleep(15 * 60)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     bot = UnconfusionBot()
     bot.run()
+
