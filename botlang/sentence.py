0858536d5f4134d1578ceddd531d8e8a5a28c310
dbaker <d-_-baker@hotmail.com>
2014-11-10 13:50:08 +1100

    guess I learnt regex! botlang can cope with smileys and punctuation afaik

--- botlang/sentence.py
+++ botlang/sentence.py
@@ -20,29 +20,55 @@ class Sentence:
             self.dictionary = json.load(fp)
 
     def translate(self, sentence):
+        entities = ["&amp;", "&lt;", "&gt;"]
+
         bot = Bot()
 
         print ("English: " + sentence)
         sentence = list(sentence.split())
         text = ""
 
-        print ("Bottish: ", end="")
         for word in sentence:
             translation = "N/A"
-            stripped_word = re.sub(r"\W+$", "", word)
 
+            # just the word, no punctuation
+            stripped_word = re.sub(r"[^a-zA-Z]", "", word)
+
+            # find the word and translate it
             for definition in self.dictionary:
                 if definition["english"] == stripped_word:
                     translation = definition["bottish"]
 
+            # no existing translation? bot learns a new word!
             if translation == "N/A":
                 translation = (bot.run(bot.gen_bottish(), word))[0]
 
-            translation = word.replace(stripped_word, translation)
+            # if the "word" was only punctuation it'll be empty
+            # so only concatenate translation if there's a word
+            if stripped_word:
 
-            text += translation + " "
+                # keep smileys, entities
+                if re.search(r"((:|;).{1,3})|(\w{1,2}(:|;))", word) or (word in entities):
+                    print ("probably a smiley, keep it");
+                    translation = word
 
-        print(text)
+                elif re.search(r"'[a-zA-Z]$", word):
+                    print("probably an abbreviation, delete punctuation")
+                    translation = word.replace(word, translation)
+
+                # replace non-punctuation part of string with translation
+                elif re.search(r"(^[^a-zA-Z])|([^a-zA-Z]$)", word):
+                    print("word contains punctuation, keep it")
+                    translation = word.replace(stripped_word, translation)
+
+                # concatenate with full sentence
+                text += translation + " "
+
+            else:
+                print("all punctuation, don't substitute anything")
+                text += word + " "
+
+        print("Bottish: " + text)
         return(text)
 
     def get_sentence(self):
